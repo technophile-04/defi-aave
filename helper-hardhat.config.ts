e2a78bff4f29fb25f79736c1943e2ed480bd7fdf
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-17 10:24:27 +0530

    add borrowing of funds and repay

--- helper-hardhat.config.ts
+++ helper-hardhat.config.ts
@@ -3,6 +3,8 @@ import { BigNumberish, ethers } from 'ethers';
 export interface networkConfigItem {
 	wEthAddress: string;
 	lendingPoolAddressProvider: string;
+	daiEthPriceFeedAddress: string;
+	daiTokenAddress: string;
 }
 
 export interface networkConfigInfo {
@@ -13,6 +15,8 @@ const networkConfig: networkConfigInfo = {
 	31337: {
 		wEthAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
 		lendingPoolAddressProvider: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+		daiEthPriceFeedAddress: '0x773616E4d11A78F511299002da57A0a94577F1f4',
+		daiTokenAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
 	},
 };
 
--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,7 +1,7 @@
 import { BigNumberish } from 'ethers';
 import { ethers, getNamedAccounts, network } from 'hardhat';
 import { networkConfig } from '../helper-hardhat.config';
-import { IERC20, ILendingPool } from '../typechain-types';
+import { AggregatorV3Interface, IERC20, ILendingPool } from '../typechain-types';
 import { ILendingPoolAddressesProvider } from '../typechain-types/contracts/interfaces/ILendingPoolAddressesProvider';
 import { ETH_DEPOSIT_AMOUNT, getWeth } from './getWETH';
 
@@ -25,6 +25,80 @@ async function main() {
 	await txnRes.wait(1);
 
 	console.log(`Deposited 🎉`);
+
+	/* Borrowing */
+	let { totalDebtETH, availableBorrowsETH } = await getBorrowUserData(lendingPool, deployer);
+	const daiPrice = await getDataPrice(networkConfig[chainId].daiEthPriceFeedAddress);
+
+	const amountDAIToBorrow =
+		parseFloat(availableBorrowsETH.toString()) * 0.95 * (1 / daiPrice.toNumber());
+
+	console.log(`You can borrow ${amountDAIToBorrow}`);
+
+	const amountDAIToBorrowWei = ethers.utils.parseEther(amountDAIToBorrow.toString());
+
+	const daiTokenAddress = networkConfig[chainId].daiTokenAddress;
+	await borrowDAI(daiTokenAddress, lendingPool, amountDAIToBorrowWei, deployer);
+
+	await getBorrowUserData(lendingPool, deployer);
+
+	await repay(amountDAIToBorrowWei, daiTokenAddress, lendingPool, deployer);
+
+	await getBorrowUserData(lendingPool, deployer);
+}
+
+async function borrowDAI(
+	daiAddress: string,
+	lendingPool: ILendingPool,
+	amountDAIToBorrowWei: BigNumberish,
+	account: string
+) {
+	const borrowTx = await lendingPool.borrow(daiAddress, amountDAIToBorrowWei, 1, 0, account);
+
+	await borrowTx.wait(1);
+
+	console.log(`You have borrowed DAI!`);
+}
+
+async function repay(
+	amount: BigNumberish,
+	daiAddress: string,
+	lendingPool: ILendingPool,
+	account: string
+) {
+	await approveERC20(daiAddress, lendingPool.address, amount, account);
+
+	const repayTx = await lendingPool.repay(daiAddress, amount, 1, account);
+
+	await repayTx.wait(1);
+
+	console.log(`Repayed !!`);
+}
+
+async function getBorrowUserData(lendingPool: ILendingPool, account: string) {
+	const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+		await lendingPool.getUserAccountData(account);
+
+	console.log(`You have ${totalCollateralETH.toString()} worth of eth deposited`);
+	console.log(`You have ${totalDebtETH.toString()} worth of eth Borrowed`);
+	console.log(`You can borrow ${availableBorrowsETH.toString()} worth of eth`);
+	return {
+		totalDebtETH,
+		availableBorrowsETH,
+	};
+}
+
+async function getDataPrice(priceFeedAddress: string) {
+	const daiEthPriceFeed = (await ethers.getContractAt(
+		'AggregatorV3Interface',
+		priceFeedAddress
+	)) as AggregatorV3Interface;
+
+	const price = (await daiEthPriceFeed.latestRoundData())[1];
+
+	console.log(`DAI/ETH price is ${ethers.utils.formatEther(price)}`);
+
+	return price;
 }
 
 async function getLendingPool(deployer: string) {
@@ -69,3 +143,15 @@ main()
 		console.log(err);
 		process.exit(1);
 	});
+
+/* 
+
+CanBorrow -> x Amount(ETH)
+
+1DAI -> 0.008ETH
+1ETH -> 1/0.008 DAI
+x ETH -> x * (1/0.008) DAI
+
+
+
+*/
